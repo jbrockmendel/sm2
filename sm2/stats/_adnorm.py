9d3d6ccbb53c6393e9a8576e6eb26a901773a40e
Brock Mendel <jbrockmendel@gmail.com>
2018-03-03 21:49:15 -0800

    flake8 cleanup

--- sm2/stats/_adnorm.py
+++ sm2/stats/_adnorm.py
@@ -6,14 +6,14 @@ Author: Josef Perktold and Scipy developers
 License : BSD-3
 """
 from __future__ import print_function
-from statsmodels.compat.python import range
+
+from six.moves import range
 import numpy as np
 from scipy import stats
 
-from numpy import exp
 
 def anderson_statistic(x, dist='norm', fit=True, params=(), axis=0):
-    '''calculate anderson-darling A2 statistic
+    """calculate anderson-darling A2 statistic
 
     Parameters
     ----------
@@ -34,9 +34,7 @@ def anderson_statistic(x, dist='norm', fit=True, params=(), axis=0):
     -------
     ad2 : float or ndarray
         Anderson-Darling statistic
-
-
-    '''
+    """
     x = np.asarray(x)
     y = np.sort(x, axis=axis)
     N = y.shape[axis]
@@ -44,12 +42,10 @@ def anderson_statistic(x, dist='norm', fit=True, params=(), axis=0):
         if dist == 'norm':
             xbar = np.expand_dims(np.mean(x, axis=axis), axis)
             s = np.expand_dims(np.std(x, ddof=1, axis=axis), axis)
-            w = (y-xbar)/s
+            w = (y - xbar) / s
             z = stats.norm.cdf(w)
-            #print z
         elif hasattr(dist, '__call__'):
             params = dist.fit(x)
-            #print params
             z = dist.cdf(y, *params)
             print(z)
     else:
@@ -58,18 +54,19 @@ def anderson_statistic(x, dist='norm', fit=True, params=(), axis=0):
         else:
             raise ValueError('if fit is false, then dist needs to be callable')
 
-    i = np.arange(1,N+1)
-    sl1 = [None]*x.ndim
+    i = np.arange(1, N + 1)
+    sl1 = [None] * x.ndim
     sl1[axis] = slice(None)
-    sl2 = [slice(None)]*x.ndim
-    sl2[axis] = slice(None,None,-1)
-    S = np.sum((2*i[sl1]-1.0)/N*(np.log(z)+np.log(1-z[sl2])), axis=axis)
-    A2 = -N-S
+    sl2 = [slice(None)] * x.ndim
+    sl2[axis] = slice(None, None, -1)
+    S = np.sum((2 * i[sl1] - 1.0) / N * (np.log(z) + np.log(1 - z[sl2])),
+               axis=axis)
+    A2 = -N - S
     return A2
 
 
 def normal_ad(x, axis=0):
-    '''Anderson-Darling test for normal distribution unknown mean and variance
+    """Anderson-Darling test for normal distribution unknown mean and variance
 
     Parameters
     ----------
@@ -84,12 +81,12 @@ def normal_ad(x, axis=0):
         pvalue for hypothesis that the data comes from a normal distribution
         with unknown mean and variance
 
-    '''
+    """
     #ad2 = stats.anderson(x)[0]
     ad2 = anderson_statistic(x, dist='norm', fit=True, axis=axis)
     n = x.shape[axis]
 
-    ad2a = ad2 * (1 + 0.75/n + 2.25/n**2)
+    ad2a = ad2 * (1 + 0.75 / n + 2.25 / n**2)
 
     if np.size(ad2a) == 1:
         if (ad2a >= 0.00 and ad2a < 0.200):
@@ -106,7 +103,7 @@ def normal_ad(x, axis=0):
     else:
         bounds = np.array([0.0, 0.200, 0.340, 0.600])
 
-        pval0 = lambda ad2a: np.nan*np.ones_like(ad2a)
+        pval0 = lambda ad2a: np.nan * np.ones_like(ad2a)
         pval1 = lambda ad2a: 1 - np.exp(-13.436 + 101.14 * ad2a - 223.73 * ad2a**2)
         pval2 = lambda ad2a: 1 - np.exp(-8.318 + 42.796 * ad2a - 59.938 * ad2a**2)
         pval3 = lambda ad2a: np.exp(0.9177 - 4.279 * ad2a - 1.38 * ad2a**2)
@@ -115,7 +112,7 @@ def normal_ad(x, axis=0):
         pvalli = [pval0, pval1, pval2, pval3, pval4]
 
         idx = np.searchsorted(bounds, ad2a, side='right')
-        pval = np.nan*np.ones_like(ad2a)
+        pval = np.nan * np.ones_like(ad2a)
         for i in range(5):
             mask = (idx == i)
             pval[mask] = pvalli[i](ad2a[mask])
