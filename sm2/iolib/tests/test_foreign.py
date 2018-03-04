c1c36223d4ddfff1e543892fbc04fb0b4d13ec27
Brock Mendel <jbrockmendel@gmail.com>
2018-03-03 16:31:37 -0800

    cleanup

--- sm2/iolib/tests/test_foreign.py
+++ sm2/iolib/tests/test_foreign.py
@@ -10,8 +10,6 @@ from numpy.testing import assert_array_equal, assert_, assert_equal, dec
 
 import pandas as pd
 import pandas.util.testing as tm
-from pandas import DataFrame, isnull
-import pandas.util.testing as ptesting
 
 from sm2.compat.testing import skipif
 
@@ -21,24 +19,18 @@ from sm2.iolib.foreign import (StataWriter, genfromdta,
 from sm2.datasets import macrodata
 
 
-from sm2.compat.pandas import version as pandas_version
-pandas_old = pandas_version < '0.9'
-
-# Test precisions
-DECIMAL_4 = 4
-DECIMAL_3 = 3
-
 curdir = os.path.dirname(os.path.abspath(__file__))
 
 '''
 def test_genfromdta():
-    #Test genfromdta vs. results/macrodta.npy created with genfromtxt.
-    #NOTE: Stata handles data very oddly.  Round tripping from csv to dta
+    # Test genfromdta vs. results/macrodta.npy created with genfromtxt.
+    # NOTE: Stata handles data very oddly.  Round tripping from csv to dta
     #    to ndarray 2710.349 (csv) -> 2510.2491 (stata) -> 2710.34912109375
     #    (dta/ndarray)
     from .results.macrodata import macrodata_result as res2
     res1 = genfromdta(curdir+'/../../datasets/macrodata/macrodata.dta')
     assert_array_equal(res1 == res2, True)
+
 
 def test_genfromdta_pandas():
     from pandas.util.testing import assert_frame_equal
@@ -48,6 +40,7 @@ def test_genfromdta_pandas():
                       pandas=True)
     res1 = res1.astype(float)
     assert_frame_equal(res1, dta)
+
 
 def test_stata_writer_structured():
     buf = BytesIO()
@@ -61,10 +54,11 @@ def test_stata_writer_structured():
     dta2 = genfromdta(buf)
     assert_array_equal(dta, dta2)
 
+
 def test_stata_writer_array():
     buf = BytesIO()
     dta = macrodata.load().data
-    dta = DataFrame.from_records(dta)
+    dta = pd.DataFrame.from_records(dta)
     dta.columns = ["v%d" % i for i in range(1,15)]
     writer = StataWriter(buf, dta.values)
     writer.write_file()
@@ -72,6 +66,7 @@ def test_stata_writer_array():
     dta2 = genfromdta(buf)
     dta = dta.to_records(index=False)
     assert_array_equal(dta, dta2)
+
 
 def test_missing_roundtrip():
     buf = BytesIO()
@@ -82,31 +77,32 @@ def test_missing_roundtrip():
     writer.write_file()
     buf.seek(0)
     dta = genfromdta(buf, missing_flt=np.nan)
-    assert_(isnull(dta[0][0]))
-    assert_(isnull(dta[0][1]))
+    assert_(pd.isnull(dta[0][0]))
+    assert_(pd.isnull(dta[0][1]))
     assert_(dta[0][2] == asbytes(""))
 
     dta = genfromdta(os.path.join(curdir, "results/data_missing.dta"),
             missing_flt=-999)
     assert_(np.all([dta[0][i] == -999 for i in range(5)]))
 
+
 def test_stata_writer_pandas():
     buf = BytesIO()
     dta = macrodata.load().data
     dtype = dta.dtype
-    #as of 0.9.0 pandas only supports i8 and f8
+    # as of 0.9.0 pandas only supports i8 and f8
     dta = dta.astype(np.dtype([('year', 'i8'),
                                ('quarter', 'i8')] + dtype.descr[2:]))
     dta4 = dta.astype(np.dtype([('year', 'i4'),
                                ('quarter', 'i4')] + dtype.descr[2:]))
-    dta = DataFrame.from_records(dta)
-    dta4 = DataFrame.from_records(dta4)
+    dta = pd.DataFrame.from_records(dta)
+    dta4 = pd.DataFrame.from_records(dta4)
     # dta is int64 'i8'  given to Stata writer
     writer = StataWriter(buf, dta)
     writer.write_file()
     buf.seek(0)
     dta2 = genfromdta(buf)
-    dta5 = DataFrame.from_records(dta2)
+    dta5 = pd.DataFrame.from_records(dta2)
     # dta2 is int32 'i4'  returned from Stata reader
 
     if dta5.dtypes[1] is np.dtype('int64'):
@@ -115,17 +111,19 @@ def test_stata_writer_pandas():
         # don't check index because it has different size, int32 versus int64
         tm.assert_frame_equal(dta4, dta5[dta5.columns[1:]])
 
+
 def test_stata_writer_unicode():
     # make sure to test with characters outside the latin-1 encoding
     pass
 
+
 def test_genfromdta_datetime():
     results = [(datetime(2006, 11, 19, 23, 13, 20), 1479596223000,
-            datetime(2010, 1, 20), datetime(2010, 1, 8), datetime(2010, 1, 1),
-            datetime(1974, 7, 1), datetime(2010, 1, 1), datetime(2010, 1, 1)),
+                datetime(2010, 1, 20), datetime(2010, 1, 8), datetime(2010, 1, 1),
+                datetime(1974, 7, 1), datetime(2010, 1, 1), datetime(2010, 1, 1)),
         (datetime(1959, 12, 31, 20, 3, 20), -1479590, datetime(1953, 10, 2),
-            datetime(1948, 6, 10), datetime(1955, 1, 1), datetime(1955, 7, 1),
-            datetime(1955, 1, 1), datetime(2, 1, 1))]
+         datetime(1948, 6, 10), datetime(1955, 1, 1), datetime(1955, 7, 1),
+         datetime(1955, 1, 1), datetime(2, 1, 1))]
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter('always')
         dta = genfromdta(os.path.join(curdir, "results/time_series_examples.dta"))
@@ -140,6 +138,7 @@ def test_genfromdta_datetime():
 
     assert_array_equal(dta.iloc[0].tolist(), results[0])
     assert_array_equal(dta.iloc[1].tolist(), results[1])
+
 
 def test_date_converters():
     ms = [-1479597200000, -1e6, -1e5, -100, 1e5, 1e6, 1479597200000]
@@ -171,7 +170,8 @@ def test_date_converters():
         assert_equal(_datetime_to_stata_elapsed(
                      _stata_elapsed_date_to_datetime(i, "ty"), "ty"), i)
 
-@skipif(pandas_old, 'pandas too old')
+
+@skipif(pd.__version__ < '0.9', 'pandas too old')
 def test_datetime_roundtrip():
     dta = np.array([(1, datetime(2010, 1, 1), 2),
                     (2, datetime(2010, 2, 1), 3),
@@ -184,7 +184,7 @@ def test_datetime_roundtrip():
     dta2 = genfromdta(buf)
     assert_equal(dta, dta2)
 
-    dta = DataFrame.from_records(dta)
+    dta = pd.DataFrame.from_records(dta)
     buf = BytesIO()
     writer = StataWriter(buf, dta, {"var2" : "tm"})
     writer.write_file()
