d557581c00490c1daf8e238957d93257da29941f
Brock Mendel <jbrockmendel@gmail.com>
2018-03-30 14:53:23 -0700

    add missing import

--- sm2/robust/tests/test_scale.py
+++ sm2/robust/tests/test_scale.py
@@ -2,6 +2,7 @@
 Test functions for models.robust.scale
 """
 
+import pytest
 import numpy as np
 from numpy.random import standard_normal
 from numpy.testing import assert_almost_equal, assert_equal
