remove unnecessary bits of statespace.tools make base class score/hessian methods work for complex and real fix reliance on self.model.method in ARResults

sm2/base/data.py
@@ -420,7 +420,7 @@ class ModelData(NullHandler):
         elif how == 'ynames':
             return self.attach_ynames(obj)
         else:
-            return obj
+            return obj  # TODO: raise on unrecognized?
 
     def attach_columns(self, result):
         return result
@@ -529,6 +529,7 @@ class PandasData(ModelData):
         # at the front, for AR lags, for example
         squeezed = result.squeeze()
         k_endog = np.array(self.ynames, ndmin=1).shape[0]
+        # TODO: is this just checking that k_endog is listlike (not stringy)?
         if k_endog > 1 and squeezed.shape == (k_endog,):
             squeezed = squeezed[None, :]
         # May be zero-dim, for example in the case of forecast one step in tsa
@@ -573,7 +574,7 @@ def _get_names(arr):
     else:
         try:
             return arr.dtype.names
-        except AttributeError:
+        except AttributeError:  # TODO: never hit; don't try/except?
             pass
 
     return None
@@ -638,7 +639,7 @@ def handle_data_class_factory(endog, exog):
         klass = ModelData
     elif data_util._is_using_pandas(endog, exog):
         klass = PandasData
-    elif data_util._is_using_patsy(endog, exog):
+    elif data_util._is_using_patsy(endog, exog):  # TODO: never used.  needed?
         klass = PatsyData
     # keep this check last
     elif data_util._is_using_ndarray(endog, exog):

sm2/tsa/regime_switching/markov_switching.py
@@ -646,6 +646,7 @@ class MarkovSwitching(tsbase.TimeSeriesModel):
     Classical and Gibbs-Sampling Approaches with Applications".
     MIT Press Books. The MIT Press.
     """
+    _use_approx_cs = True
 
     @property
     def _res_classes(self):
@@ -1141,56 +1142,6 @@ class MarkovSwitching(tsbase.TimeSeriesModel):
         results = self._filter(params)
 
         return np.log(results[5])
-
-    # TODO: Just use default implementation from base class?  the cs part is
-    # different
-    def score(self, params, transformed=True):
-        """
-        Compute the score function at params.
-
-        Parameters
-        ----------
-        params : array_like
-            Array of parameters at which to evaluate the score
-            function.
-        transformed : boolean, optional
-            Whether or not `params` is already transformed. Default is True.
-        """
-        params = np.array(params, ndmin=1)
-        return approx_fprime_cs(params, self.loglike, args=(transformed,))
-
-    # TODO: Just use default implementation from base class?  the cs part is
-    # different
-    def score_obs(self, params, transformed=True):
-        """
-        Compute the score per observation, evaluated at params
-
-        Parameters
-        ----------
-        params : array_like
-            Array of parameters at which to evaluate the score
-            function.
-        transformed : boolean, optional
-            Whether or not `params` is already transformed. Default is True.
-        """
-        params = np.array(params, ndmin=1)
-        return approx_fprime_cs(params, self.loglikeobs, args=(transformed,))
-
-    def hessian(self, params, transformed=True):
-        """
-        Hessian matrix of the likelihood function, evaluated at the given
-        parameters
-
-        Parameters
-        ----------
-        params : array_like
-            Array of parameters at which to evaluate the Hessian
-            function.
-        transformed : boolean, optional
-            Whether or not `params` is already transformed. Default is True.
-        """
-        params = np.array(params, ndmin=1)
-        return approx_hess_cs(params, self.loglike)
 
     def fit(self, start_params=None, transformed=True, cov_type='approx',
             cov_kwds=None, method='bfgs', maxiter=100, full_output=1, disp=0,