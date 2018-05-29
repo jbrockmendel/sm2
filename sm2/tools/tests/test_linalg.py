9048f405ad712efeda41da7d099baefec1b961d2
Brock Mendel <jbrockmendel@gmail.com>
2018-05-29 11:21:56 -0700

    Add randomized tests for smw_solver, smw_logdet

--- sm2/tools/tests/test_linalg.py
+++ sm2/tools/tests/test_linalg.py
@@ -112,14 +112,14 @@ def test_smw_solver():
     p = 5
     q = 4
     r = 2
-    s = 2
+    d = 2
 
     A = np.random.normal(size=(p, q))
     AtA = np.dot(A.T, A)
 
     B = np.zeros((q, q))
     B[0:r, 0:r] = np.random.normal(size=(r, r))
-    di = np.random.uniform(size=s)
+    di = np.random.uniform(size=d)
     B[r:q, r:q] = np.diag(1 / di)
     Qi = np.linalg.inv(B[0:r, 0:r])
     s = 0.5
@@ -136,13 +136,45 @@ def test_smw_solver():
     assert_allclose(y1, y2)
 
 
+@pytest.mark.parametrize('n', range(100))
+def test_smw_solver_randomized(n):
+    # github.com/statsmodels/statsmodels/pull/4594#issuecomment-392286075
+    p = np.random.randint(1, 5)
+    # p == 0 is not meaningful
+    r = np.random.randint(1, 5)
+    d = np.random.randint(1, 5)
+    q = r + d  # in general we expect q << p (see docstring)
+    p = q + np.random.randint(1, 5)
+
+    A = np.random.normal(size=(p, q))
+    AtA = np.dot(A.T, A)
+
+    B = np.zeros((q, q))
+    B[0:r, 0:r] = np.random.normal(size=(r, r))
+    di = np.random.uniform(size=d)
+    B[r:q, r:q] = np.diag(1 / di)
+    Qi = np.linalg.inv(B[0:r, 0:r])
+    s = np.random.random()  # upstream uses 0.5, requirement is just s>0
+
+    x = np.random.normal(size=p)
+    y2 = np.linalg.solve(s * np.eye(p, p) + np.dot(A, np.dot(B, A.T)), x)
+
+    f = smw_solver(s, A, AtA, Qi, di)
+    y1 = f(x)
+    assert_allclose(y1, y2)
+
+    f = smw_solver(s, sparse.csr_matrix(A), sparse.csr_matrix(AtA), Qi, di)
+    y1 = f(x)
+    assert_allclose(y1, y2, err_msg=str((p, r, q)))
+
+
 def test_smw_logdet():
     # GH#4594
     np.random.seed(23)
     p = 5
     q = 4
     r = 2
-    s = 2
+    d = 2
 
     A = np.random.normal(size=(p, q))
     AtA = np.dot(A.T, A)
@@ -150,7 +182,7 @@ def test_smw_logdet():
     B = np.zeros((q, q))
     c = np.random.normal(size=(r, r))
     B[0:r, 0:r] = np.dot(c.T, c)
-    di = np.random.uniform(size=s)
+    di = np.random.uniform(size=d)
     B[r:q, r:q] = np.diag(1 / di)
     Qi = np.linalg.inv(B[0:r, 0:r])
     s = 0.5
@@ -160,3 +192,32 @@ def test_smw_logdet():
     _, bd = np.linalg.slogdet(B)
     d1 = smw_logdet(s, A, AtA, Qi, di, bd)
     assert_allclose(d1, d2)
+
+
+@pytest.mark.parametrize('n', range(100))
+def test_smw_logdet_randomized(n):
+    # github.com/statsmodels/statsmodels/pull/4594#issuecomment-392286075
+    r = np.random.randint(1, 5)
+    d = np.random.randint(1, 5)
+    q = r + d  # in general we expect q << p (see docstring)
+    p = q + np.random.randint(1, 5)
+
+    A = np.random.normal(size=(p, q))
+    AtA = np.dot(A.T, A)
+
+    B = np.zeros((q, q))
+    # Note this is different from the smw_solver construction.  Here we need
+    # B to be positive-semidefinite, but for smw_solver it only
+    # needs to be non-singular
+    c = np.random.normal(size=(r, r))
+    B[0:r, 0:r] = np.dot(c.T, c)
+    di = np.random.uniform(size=d)
+    B[r:q, r:q] = np.diag(1 / di)
+    Qi = np.linalg.inv(B[0:r, 0:r])
+    s = np.random.random()  # upstream uses 0.5, requirement is just s>0
+
+    _, d2 = np.linalg.slogdet(s * np.eye(p, p) + np.dot(A, np.dot(B, A.T)))
+
+    _, bd = np.linalg.slogdet(B)
+    d1 = smw_logdet(s, A, AtA, Qi, di, bd)
+    assert_allclose(d1, d2, atol=1e-12, err_msg=str((p, r, q)))
