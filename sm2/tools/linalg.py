8f15de802bd95d7855a71b3129272aa288c2c3de
Brock Mendel <jbrockmendel@gmail.com>
2018-09-23 20:05:59 -0700

    Remove shims deleted upstream

--- sm2/tools/linalg.py
+++ sm2/tools/linalg.py
@@ -7,29 +7,13 @@ changes:
 The only changes are that additional results are returned
 
 """
-from six.moves import reduce
+from six.moves import reduce, range
 
 import numpy as np
 import scipy.linalg
 from scipy import sparse
 import scipy.sparse.linalg  # needed to get linalg into sparse namespace
-
-eps = np.finfo(float).eps
-feps = np.finfo(np.single).eps
-_array_precision = {'f': 0, 'd': 1, 'F': 0, 'D': 1}
-
-
-# Linear Least Squares
-def lstsq(a, b, cond=None, overwrite_a=0, overwrite_b=0):  # pragma: no cover
-    raise NotImplementedError("lstsq not ported from upstream")
-
-
-def pinv(a, cond=None, rcond=None):  # pragma: no cover
-    raise NotImplementedError("pinv not ported from upstream")
-
-
-def pinv2(a, cond=None, rcond=None):  # pragma: no cover
-    raise NotImplementedError("pinv2 not ported from upstream")
+from scipy.linalg import pinv, pinv2, lstsq  # noqa:F421
 
 
 def logdet_symm(m, check_symm=False):
